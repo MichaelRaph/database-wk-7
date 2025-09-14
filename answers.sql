eb06f0994c7511989170110ddbfb0582ed59e340
MichaelRaph <robertwilliamsdon00@gmail.com>
2025-09-14 17:02:28 +0100

    DATABASE WEEK7

---
+++ answers.sql
@@ -0,0 +1,55 @@
+-- QUESTION 1 Create new table in 1NF
+
+CREATE TABLE ProductDetail_1NF (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(100)
+);
+
+-- Insert atomic values (1 product per row)
+INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product) VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+
+
+-- QUESTION 2 Achieving 2NF (Second Normal Form) 
+--Given OrderDetails table, 
+--which is already in 1NF but still contains partial dependencies:
+
+-- Orders table: Customer depends only on OrderID
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+-- OrderProducts table: Product and Quantity depend on OrderID + Product
+CREATE TABLE OrderProducts (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+-- Insert into Orders
+INSERT INTO Orders (OrderID, CustomerName) VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+-- Insert into OrderProducts
+INSERT INTO OrderProducts (OrderID, Product, Quantity) VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
+
+
+
